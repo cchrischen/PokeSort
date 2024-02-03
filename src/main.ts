79e4eb075ffa65857d1b19e0147028c857b8fac3
Chris Chen <cchen6479@gmail.com>
2024-02-03 18:23:18 -0500

    vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import './assets/styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import { inject } from '@vercel/analytics';
+ 
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { PiUnownQuestion } from "oh-vue-icons/icons";
 
 addIcons(PiUnownQuestion);
+inject();
 
 const app = createApp(App);
 app.component("v-icon", OhVueIcon);
